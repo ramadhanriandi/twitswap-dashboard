72d7b3bf77502981aca7aea27fc4b59dfa519cb1
ramadhanriandi <izurbudianto@gmail.com>
2021-02-19 07:05:06 +0700

    display total tweet count

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <img src="../assets/logo.png" alt="Twitter" />
     </div>
     <p class="description">
-      Stream any tweets related to your inputted contexts below <span>(up to 5 contexts)</span>
+      Stream any tweets related to your inputted keywords below <span>(up to 5 keywords)</span>
     </p>
 
     <form @submit.prevent="handleStreaming" method="post">
@@ -13,23 +13,33 @@
         <b-form-tags
           :disabled="isStreaming"
           :limit="limit"
-          placeholder="Type a tag and push enter to input it"
+          placeholder="Type a keyword and push enter to input it"
           remove-on-delete size="lg"
           tag-pills
           tag-variant="primary"
-          v-model="contexts"
+          v-model="keywords"
         />
       </div>
       <div class="d-flex justify-content-end mb-4">
         <b-button v-if="isStreaming" type="submit" variant="danger">S T O P</b-button>
-        <b-button :disabled="contexts.length === 0" type="submit" v-else :variant="contexts.length ? 'success' : ''">
+        <b-button :disabled="keywords.length === 0" type="submit" v-else :variant="keywords.length ? 'success' : ''">
           S T A R T
         </b-button>
       </div>
     </form>
 
+    <div class="row justify-content-between align-items-center mb-5">
+      <Overview class="col" :startTime="overview.startTime" :totalTweetCount="overview.totalTweetCount" />
+      <PublicMetrics
+        class="col"
+        :likeCount="publicMetric.likeCount"
+        :replyCount="publicMetric.replyCount"
+        :retweetCount="publicMetric.retweetCount"
+      />
+    </div>
+
     <div class="d-flex justify-content-between align-items-center">
-      <PopularHashtags v-if="count % 2 === 1" :popularHashtags="popularHashtags" />
+      <PopularHashtags v-if="dataLoadCount % 2 === 1" :popularHashtags="popularHashtags" />
       <PopularHashtags v-else :popularHashtags="popularHashtags" />
     </div>
   </div>
@@ -37,24 +47,39 @@
 
 <script>
 import axios from 'axios';
+
+import Overview from '../components/Overview';
 import PopularHashtags from '../components/PopularHashtags';
+import PublicMetrics from '../components/PublicMetrics';
+
 import Configs from '../constants/config';
 
 export default {
   components: {
+    Overview,
     PopularHashtags,
+    PublicMetrics,
   },
   data: () => ({
-    contexts: [],
+    dataLoadCount: 0,
     isStreaming: false,
+    keywords: [],
     limit: 5,
     polling: null,
     popularHashtags: {},
-    count: 0,
+    publicMetric: {
+      likeCount: 0,
+      replyCount: 0,
+      retweetCount: 0,
+    },
+    overview: {
+      startTime: null,
+      totalTweetCount: 0,
+    }
   }),
   methods: {
     async handleStreaming() {
-      const requestBody = JSON.stringify({ tags: this.contexts });
+      const requestBody = JSON.stringify({ tags: this.keywords });
       const response = await axios.post(
         `${Configs.API_SERVER_URL}/v1/streaming?stop=${this.isStreaming}`,
         requestBody,
@@ -66,6 +91,12 @@ export default {
       }
 
       if (this.isStreaming) {
+        this.overview.startTime = new Date();
+
+        this.publicMetric.likeCount += 1;
+        this.publicMetric.replyCount += 1;
+        this.publicMetric.retweetCount += 1;
+
         this.loadData();
       } else {
         clearInterval(this.polling);
@@ -80,11 +111,14 @@ export default {
         
         if (response.status === 200) {
           const { data } = response.data;
-          this.popularHashtags = data.popularHashtags; 
+          const { popularHashtags, tweetCount } = data;
+          
+          this.popularHashtags = popularHashtags; 
+          this.overview.totalTweetCount = tweetCount.data;
         }
 
-        this.count += 1;
-      }, 2000);
+        this.dataLoadCount += 1;
+      }, 10000);
     }
   },
 };
